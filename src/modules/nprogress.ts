72ab253b4003f672bc901821f3dffc83bbf40aaa
xiaojun996 <blueskyzx@icloud.com>
2021-06-19 15:49:31 +0800

    chore

--- src/modules/nprogress.ts
+++ src/modules/nprogress.ts
@@ -1,6 +1,6 @@
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
-import { UserModule } from '/@/types'
+import type { UserModule } from '/@/types'
 
 export const install: UserModule = ({ isClient, router }) => {
   if (isClient) {
