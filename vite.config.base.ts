eb63d8393f3b03698f9520521e4bd671f40908c7
xiaojun996 <blueskyzx@icloud.com>
2021-06-19 15:17:08 +0800

    chore: remove

--- vite.config.base.ts
+++ vite.config.base.ts
@@ -46,10 +46,6 @@ export const userConfig = defineConfig({
         find: '/@',
         replacement: resolve(__dirname, './src'),
       },
-      {
-        find: '/#',
-        replacement: resolve(__dirname, './types'),
-      },
     ],
   },
   css: {
