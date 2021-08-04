5837172fb0cba217a7cd73e1696076b8b18e3ccb
zhixiangyao <blueskyzx@icloud.com>
2021-08-04 14:37:24 +0800

    feat: add animation

--- src/plugins/Github.vue
+++ src/plugins/Github.vue
@@ -1,12 +1,12 @@
 <template>
-  <a :href="href" target="_blank" title="Visit the open-source code on GitHub!">
+  <a :href="href" target="_blank" class="github-corner" aria-label="View source on GitHub">
     <svg
       class="
         fill-black
         dark:fill-white dark:text-black
         text-white
-        w-20
-        h-20
+        w-16
+        h-16
         absolute
         top-0
         right-0
@@ -20,6 +20,7 @@
       <path
         d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2"
         fill="currentColor"
+        style="transform-origin: 130px 106px"
         class="octo-arm"
       />
       <path
@@ -49,3 +50,36 @@ export default defineComponent({
   },
 })
 </script>
+
+<style scoped>
+.github-corner:hover .octo-arm {
+  animation: octocat-wave 560ms ease-in-out;
+}
+
+@keyframes octocat-wave {
+  0%,
+  100% {
+    transform: rotate(0);
+  }
+
+  20%,
+  60% {
+    transform: rotate(-25deg);
+  }
+
+  40%,
+  80% {
+    transform: rotate(10deg);
+  }
+}
+
+@media (max-width: 500px) {
+  .github-corner:hover .octo-arm {
+    animation: none;
+  }
+
+  .github-corner .octo-arm {
+    animation: octocat-wave 560ms ease-in-out;
+  }
+}
+</style>
