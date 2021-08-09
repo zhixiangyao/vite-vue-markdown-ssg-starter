38c626937a53e8feca4e58a34b72e0a256c0acd5
zhixiangyao <blueskyzx@icloud.com>
2021-08-09 14:42:50 +0800

    chore

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,3 @@
-/// <reference types="vite/client" />
-/// <reference types="vite-plugin-pages/client" />
-
 /**
  * https://cn.vitejs.dev/guide/env-and-mode.html#intellisense
  *
@@ -10,6 +7,7 @@
  * 你可能想要在代码中获取这些以 'VITE_' 为前缀的用户自定义 env variable 的 TypeScript 智能提示。
  * 要想做到这一点，你可以在 src 目录下创建一个 env.d.ts file，
  * 接着按下面这样增加 ImportMetaEnv 的定义：
+ * ImportMetaEnv 会和 tsconfig.json -> compilerOptions -> types = ["vite/client"] 里的 ImportMetaEnv 合并
  */
 
 interface ImportMetaEnv {
