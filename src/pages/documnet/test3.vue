c4304c051245447f24a3dc963b06bcfee5301d1a
zhixiangyao <blueskyzx@icloud.com>
2022-02-07 10:46:56 +0800

    style: new prettier rule

--- src/pages/documnet/test3.vue
+++ src/pages/documnet/test3.vue
@@ -24,18 +24,7 @@ function add() {
   <div>temp: {{ temp.count }}</div>
 
   <button
-    class="
-      my-4
-      py-2
-      px-4
-      bg-indigo-500
-      text-white
-      font-semibold
-      rounded-lg
-      shadow-md
-      hover:bg-indigo-700
-      focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-opacity-75;
-    "
+    class="my-4 py-2 px-4 bg-indigo-500 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-400 focus:ring-opacity-75;"
     @click="add()"
   >
     Click
