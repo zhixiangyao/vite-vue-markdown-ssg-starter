efba6dab1eada824c63e79416e9806a2248fa314
zhixiangyao <blueskyzx@icloud.com>
2024-05-20 14:09:46 +0800

    feat: update deps & other

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,26 @@
 import { ViteSSG } from 'vite-ssg'
+import generatedRoutes from 'virtual:generated-pages'
+import { setupLayouts } from 'virtual:generated-layouts'
+
+import App from '/@/App.vue'
+
 /**
  * vite-plugin-pages:         Generating routing information
  * virtual:generated-pages:   It comes from tsconfig.json-compilerOptions-types -> "vite-plugin-pages/client"
  * virtual:generated-layouts: It comes from tsconfig.json-compilerOptions-types -> "vite-plugin-vue-layouts/client"
  */
-import generatedRoutes from 'virtual:generated-pages'
-import { setupLayouts } from 'virtual:generated-layouts'
 import 'virtual:windi.css'
 import 'virtual:windi-devtools'
 import '/@/styles/font.css'
 import '/@/styles/main.css'
-import App from '/@/App.vue'
 
 const routes = setupLayouts(generatedRoutes)
 
+type Module = Record<string, Record<string, UserModule>>
+
 export const createApp = ViteSSG(App, { routes }, (ctx) => {
+  const modules: Module = import.meta.glob('./modules/*.ts', { eager: true })
+
   // install all modules under `modules/`
-  Object.values(import.meta.globEager('./modules/*.ts')).map((i) => i.install?.(ctx))
+  Object.values(modules).map((m) => m.install?.(ctx))
 })
