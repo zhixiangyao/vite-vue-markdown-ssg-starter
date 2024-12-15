1455b6cc7f570d951f0481307e9c29f90526f17a
zhixiangyao <blueskyzx@icloud.com>
2024-12-15 13:53:36 +0800

    chore

--- src/global.d.ts
+++ src/global.d.ts
@@ -17,20 +17,7 @@ declare global {
 
   type UserModule = (ctx: ViteSSGContext) => void
 
-  /**
-   * https://cn.vitejs.dev/guide/env-and-mode.html#intellisense
-   *
-   * 智能提示
-   * 默认情况下，Vite 为 import.meta.env 提供了 Type 定义。
-   * 随着在 .env[mode] file 中自定义了越来越多的 env variable ，
-   * 你可能想要在代码中获取这些以 'VITE_' 为前缀的用户自定义 env variable 的 TypeScript 智能提示。
-   * 要想做到这一点，你可以在 src 目录下创建一个 env.d.ts file，
-   * 接着按下面这样增加 ImportMetaEnv 的定义：
-   * ImportMetaEnv 会和 tsconfig.json -> compilerOptions -> types = ["vite/client"] 里的 ImportMetaEnv 合并
-   */
-
   interface ImportMetaEnv {
-    VITE_APP_NODE_ENV: string
     VITE_APP_ENV: string
     VITE_APP_PROXY_URL: string
     VITE_APP_BASE_URL: string
--- vite.config.ts
+++ vite.config.ts
@@ -94,16 +94,15 @@ const baseConfig: UserConfigExport = {
 export default ({ command, mode }: ConfigEnv) => {
   /**
    * Such as:
-   * import.meta.env.MODE: {string}       app runtime 的模式。
-   * import.meta.env.BASE_URL: {string}   部署 app 时的基本 URL 。他由 base 配置项决定。
-   * import.meta.env.PROD: {boolean}      app 是否 runtime 在生产环境。
-   * import.meta.env.DEV: {boolean}       app 是否 runtime 在开发环境 (永远与 import.meta.env.PROD 相反)。
+   * import.meta.env.MODE: {string}       The mode of the app runtime.
+   * import.meta.env.BASE_URL: {string}   The base URL for deploying the app. This is determined by the base configuration entry.
+   * import.meta.env.PROD: {boolean}      Whether the app is runtime in the production environment.
+   * import.meta.env.DEV: {boolean}       Whether app runtime is in the development environment (always the opposite of import.meta.env.PROD).
    */
-  const { VITE_APP_NODE_ENV, VITE_APP_ENV, VITE_APP_PROXY_URL, VITE_APP_BASE_URL } = getEnv(mode)
+  const { VITE_APP_ENV, VITE_APP_PROXY_URL, VITE_APP_BASE_URL } = getEnv(mode)
 
   setTimeout(() => {
     console.log()
-    console.log('\x1b[33m%s\x1b[0m', `🏭--NODE环境(VITE_APP_NODE_ENV): ${VITE_APP_NODE_ENV}`)
     console.log('\x1b[36m%s\x1b[0m', `🏠--APP环境(VITE_APP_ENV): ${VITE_APP_ENV}`)
     console.log('\x1b[36m%s\x1b[0m', `😈--APP代理URL(VITE_APP_PROXY_URL): ${VITE_APP_PROXY_URL}`)
     console.log('\x1b[36m%s\x1b[0m', `🔗--APP基础URL(VITE_APP_BASE_URL): ${VITE_APP_BASE_URL}`)
